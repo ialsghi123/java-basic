f9f53112bded03603734d75cb22759f5fd0684f1
ialsghi123 <ialsghi123@naver.com>
2021-01-18 23:14:01 +0900

    2021-01-18

--- src/practice/FruitBoxEx2.java
+++ src/practice/FruitBoxEx2.java
@@ -1,81 +1,81 @@
-package practice;
-
-import java.util.ArrayList;
-
-class Fruit implements Eatable {
-    @Override
-    public String toString() {
-        return "Fruit";
-    }
-}
-
-class Apple extends Fruit {
-    @Override
-    public String toString() {
-        return "Apple";
-    }
-}
-
-class Grape extends Fruit {
-    @Override
-    public String toString() {
-        return "Grape";
-    }
-}
-
-class Toy {
-    @Override
-    public String toString() {
-        return "Toy";
-    }
-}
-
-interface Eatable {}
-
-
-public class FruitBoxEx2 {
-
-
-    public static void main(String[] args) {
-        FruitBox<Fruit> fruitBox = new FruitBox<Fruit>();
-        FruitBox<Apple> appleBox = new FruitBox<Apple>();
-        FruitBox<Grape> grapeBox = new FruitBox<Grape>();
-
-        fruitBox.add(new Fruit());
-        fruitBox.add(new Apple());
-        fruitBox.add(new Grape());
-        grapeBox.add(new Grape());
-
-
-        System.out.println(fruitBox);
-        System.out.println(appleBox);
-        System.out.println(grapeBox);
-    }
-}
-
-
-class FruitBox<T extends Fruit & Eatable> extends Box<T> {
-
-}
-
-class Box<T> {
-    ArrayList<T> list = new ArrayList<T>();
-    void add(T item) {
-        list.add(item);
-    }
-
-    T get(int i) {
-        return list.get(i);
-    }
-
-    int size() {
-        return list.size();
-    }
-
-
-    public String toString() {
-        return list.toString();
-    }
-}
-
-
+//package practice;
+//
+//import java.util.ArrayList;
+//
+//class Fruit implements Eatable {
+//    @Override
+//    public String toString() {
+//        return "Fruit";
+//    }
+//}
+//
+//class Apple extends Fruit {
+//    @Override
+//    public String toString() {
+//        return "Apple";
+//    }
+//}
+//
+//class Grape extends Fruit {
+//    @Override
+//    public String toString() {
+//        return "Grape";
+//    }
+//}
+//
+//class Toy {
+//    @Override
+//    public String toString() {
+//        return "Toy";
+//    }
+//}
+//
+//interface Eatable {}
+//
+//
+//public class FruitBoxEx2 {
+//
+//
+//    public static void main(String[] args) {
+//        FruitBox<Fruit> fruitBox = new FruitBox<Fruit>();
+//        FruitBox<Apple> appleBox = new FruitBox<Apple>();
+//        FruitBox<Grape> grapeBox = new FruitBox<Grape>();
+//
+//        fruitBox.add(new Fruit());
+//        fruitBox.add(new Apple());
+//        fruitBox.add(new Grape());
+//        grapeBox.add(new Grape());
+//
+//
+//        System.out.println(fruitBox);
+//        System.out.println(appleBox);
+//        System.out.println(grapeBox);
+//    }
+//}
+//
+//
+//class FruitBox<T extends Fruit & Eatable> extends Box<T> {
+//
+//}
+//
+//class Box<T> {
+//    ArrayList<T> list = new ArrayList<T>();
+//    void add(T item) {
+//        list.add(item);
+//    }
+//
+//    T get(int i) {
+//        return list.get(i);
+//    }
+//
+//    int size() {
+//        return list.size();
+//    }
+//
+//
+//    public String toString() {
+//        return list.toString();
+//    }
+//}
+//
+//
